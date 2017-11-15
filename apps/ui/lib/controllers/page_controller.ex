5125660f41e9e80926bc7f211479a7c47ccd021a
Ilona Koda <ilona.koda@amberbit.com>
2017-11-15 21:21:19 +0100

    Remove require

--- apps/ui/lib/controllers/page_controller.ex
+++ apps/ui/lib/controllers/page_controller.ex
@@ -1,8 +1,6 @@
 defmodule Ui.PageController do
   use Ui, :controller
 
-  require Backend.FortuneServer
-
   def index(conn, _params) do
     render conn, "index.html", quote: Backend.FortuneServer.fetch_random_quote 
   end
