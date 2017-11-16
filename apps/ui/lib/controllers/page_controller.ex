80dce48d73adeb741947d85a006fd1fcdc3bf007
aneta-bielska <aneta.bielska@amberbit.com>
2017-11-16 12:47:36 +0100

    rename module

--- apps/ui/lib/controllers/page_controller.ex
+++ apps/ui/lib/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule Ui.PageController do
   use Ui, :controller
 
   def index(conn, _params) do
-    render conn, "index.html", quote: Ui.FortunesClient.fetch_random_quote
+    render conn, "index.html", quote: Ui.FortuneClient.fetch_random_quote
   end
 end
--- apps/ui/lib/fortune_client.ex
+++ apps/ui/lib/fortune_client.ex
@@ -1,4 +1,4 @@
-defmodule Ui.FortunesClient do
+defmodule Ui.FortuneClient do
   def fetch_random_quote do
     GenServer.call(Backend.FortuneServer, :fetch_random_quote)
   end
