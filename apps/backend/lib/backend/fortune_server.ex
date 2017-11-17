9e14a5aa98bce7c18d2f74ab40ccf1ab29036d0b
Hubert Łępicki <hubert.lepicki@amberbit.com>
2017-11-17 13:40:00 +0100

    extract quotes to own module

--- apps/backend/lib/backend/fortune_server.ex
+++ apps/backend/lib/backend/fortune_server.ex
@@ -5,35 +5,17 @@ defmodule Backend.FortuneServer do
     GenServer.start_link(__MODULE__, :ok, name: {:global, __MODULE__})
   end
 
+  @quotes_db Backend.HardcodedQuotesDatabase
+
   def init(:ok) do
-    quotes = [%{
-      quote: "It is better to be making the news than taking it; to be an actor rather than a critic.",
-      author: "Winston Churchill"
-    }, %{
-      quote: "To improve is to change, so to be perfect is to have changed often.",
-      author: "Winston Churchill"
-    }, %{
-      quote: "Attitude is a little thing that makes a big difference.",
-      author: "Winston Churchill"
-    }, %{
-      quote: "You have enemies? Good. That means you've stood up for something, sometime in your life.",
-      author: "Winston Churchill"
-    }, %{
-      quote: "The best argument against democracy is a five-minute conversation with the average voter.",
-      author: "Winston Churchill"
-    }]
-
-    {:ok, quotes}
+    {:ok, @quotes_db}
   end
 
-  def handle_call(:fetch_random_quote, _from, quotes) do
-    {:reply, {:ok, Enum.random(quotes)}, quotes}
+  def handle_call(:fetch_random_quote, _from, quotes_db) do
+    {:reply, quotes_db.random(), quotes_db}
   end
 
-  def handle_call({:fetch_quote, index}, _from, quotes) do
-    case Enum.at(quotes, index, :not_found) do
-      :not_found -> {:reply, {:error, :not_found}, quotes}
-      found_quote -> {:reply, {:ok, found_quote}, quotes}
-    end
+  def handle_call({:fetch_quote, index}, _from, quotes_db) do
+    {:reply, quotes_db.at_index(index), quotes_db}
   end
 end
---
+++ apps/backend/lib/backend/hardcoded_quotes_database.ex
@@ -0,0 +1,30 @@
+defmodule Backend.HardcodedQuotesDatabase do
+    @quotes [%{
+      quote: "It is better to be making the news than taking it; to be an actor rather than a critic.",
+      author: "Winston Churchill"
+    }, %{
+      quote: "To improve is to change, so to be perfect is to have changed often.",
+      author: "Winston Churchill"
+    }, %{
+      quote: "Attitude is a little thing that makes a big difference.",
+      author: "Winston Churchill"
+    }, %{
+      quote: "You have enemies? Good. That means you've stood up for something, sometime in your life.",
+      author: "Winston Churchill"
+    }, %{
+      quote: "The best argument against democracy is a five-minute conversation with the average voter.",
+      author: "Winston Churchill"
+    }]
+
+
+  def random() do
+    {:ok, Enum.random(@quotes)}
+  end
+
+  def at_index(index) do
+    case Enum.at(@quotes, index) do
+      nil -> {:error, :not_found}
+      quote_at_index -> {:ok, quote_at_index}
+    end
+  end
+end
